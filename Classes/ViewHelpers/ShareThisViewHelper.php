1fc62e33afa702f5a57c428f8785b9012317eecd
Jonathan Pelchat <jonathan.pelchat.2@ulaval.ca>
2023-06-20 11:52:18 -0400

    Change the base class of the ShareThisViewHelper to the correct one

--- Classes/ViewHelpers/ShareThisViewHelper.php
+++ Classes/ViewHelpers/ShareThisViewHelper.php
@@ -12,13 +12,13 @@ use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
  *
  * @author SEVER26
  */
-class ShareThisViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper 
+class ShareThisViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper
 {
     /**
      * @var boolean
      */
     protected $escapeOutput = false;
-    
+
     /**
      * Initialize arguments
      *
@@ -36,46 +36,46 @@ class ShareThisViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagB
         $url = $arguments['url'];
 
         $configuration = isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ns_sharethis']) ? unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ns_sharethis']) : '';
-        
+
         if($socials === ""){
             $socials = $configuration['socials'];
         }
-        
+
         $socials = explode(',',$socials);
         foreach($socials as $index => $social){
             if(!in_array($social, Utility::ALLOWED_SOCIALS)){
                 unset($socials[$index]);
             }
         }
-        
+
         if(!is_array($socials) OR (is_array($socials) AND count($socials) === 0)){
             //TODO use translate
             $content = "invalid socials";
         }
         else{
-            
+
             $data = array(
                 'socials' => $socials,
                 'url' => $url,
             );
-            
+
             $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
             $pageRenderer->addHeaderData(Utility::getPublicJsRessourcesHtmlTags());
-            
+
             $partialPath = 'EXT:ns_sharethis/Resources/Private/Partials/ShareButtons.html';
             $variables = $templateVariableContainer->getByPath('settings');
             $customPartialsPath = $variables['ns_sharethis']['customPartial'] ?? NULL;
             if($customPartialsPath !== NULL){
                 $partialPath = $customPartialsPath;
             }
-            
+
             $view = GeneralUtility::makeInstance('TYPO3\\CMS\\Fluid\\View\\StandaloneView');
             $view->setTemplatePathAndFilename($partialPath);
             $view->assignMultiple($data);
 
             $content = $view->render();
         }
-        
+
 
         return $content;
     }
